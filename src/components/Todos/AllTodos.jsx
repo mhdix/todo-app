2d20ba305b8d044638efb36b7d1c4fb0a1ee5f5a
mahdi <domanloom@gmail.com>
2023-09-15 20:15:01 +0330

    fixed allTodos style

--- src/components/Todos/AllTodos.jsx
+++ src/components/Todos/AllTodos.jsx
@@ -4,7 +4,25 @@ const AllTodos = ({ todos, setTodos }) => {
   console.log(todos);
   return (
     <div className='all-todos'>
-      <div>
+      <div className='todo'>
+        <p>1</p>
+        <h3>موضوع</h3>
+        <p>شماره 1</p>
+        <div className='example'>
+          <h3>توضیحات</h3>
+          <p>شماره 1</p>
+        </div>
+      </div>
+      <div className='todo'>
+        <p>1</p>
+        <h3>موضوع</h3>
+        <p>شماره 1</p>
+        <div className='example'>
+          <h3>توضیحات</h3>
+          <p>شماره 1</p>
+        </div>
+      </div>
+      <div className='todo'>
         <p>1</p>
         <h3>موضوع</h3>
         <p>شماره 1</p>
